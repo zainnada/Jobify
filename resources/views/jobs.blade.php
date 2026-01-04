f09ec7b851eca0258513e0eb9047fb836346146f
Zain Hasan <zain-nada-12@hotmail.com>
2026-01-04 20:10:41 +0200

    fix: using Eager Loading to fix N+1 problem, and Disable Lazy Loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,14 +4,15 @@
         Job Listings
     </x-slot:heading>
 
-    <ul>
+    <div class="space-y-4">
         @foreach($jobs as $job)
-            <li>
-                <a href="/jobs/{{$job['id']}}" class="text-blue-500 hover:underline">
+            <a href="/jobs/{{$job['id']}}" class="block px-4 py-6 border border-gray-200 rounded-lg">
+                <div class="font-bold text-blue-500 text-sm">
+                    {{ $job->employer->name }}
+                </div>
                 <strong>{{$job['title']}}:</strong> Pays {{$job['salary']}} per year.
-                </a>
-            </li>
+            </a>
         @endforeach
-    </ul>
+    </div>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 Route::get('/jobs', function () {
     return view('jobs',
         [
-            'jobs' => Job::all()
+            'jobs' => Job::with('employer')->get()
         ]);
 });
 
